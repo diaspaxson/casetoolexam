a43ef318632ec22c00aca71d44de716949c8e4e1
exam <exam@Bcalab4-m15>
2023-05-04 09:25:17 +0530

    First commit

---
+++ exam/src/exam/book_216.java
@@ -0,0 +1,18 @@
+package exam;
+
+public class book_216 {
+	int page_num;
+	int total_pages;
+	
+	book_216(int total_pages)
+	{
+		this.total_pages=total_pages;
+		this.page_num=1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
